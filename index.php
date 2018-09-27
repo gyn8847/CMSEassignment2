1670b1a2d6af2e6013cd0595b2f33905d0b137d1
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-09-27 14:30:28 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <html>
 <?php
-  array parse_ini_file ( string $css/php.ini [, bool $process_sections = FALSE [, int $scanner_mode = INI_SCANNER_NORMAL ]] )
-    
   header("location:css/index.php");
 ?>
 </html>
