0b214a34d71af941e542d5b20bcea2eaa28f685b
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-09-27 14:11:03 +1200

    Update index.php

--- css/index.php
+++ css/index.php
@@ -41,6 +41,9 @@
 									<span class="total_price">$ 0.00</span>
 								</a>
 							</li>
+							<form action="thingsOrIdeas.php">
+	<button type="submit" formaction = "image.html" style="margin-bottom: 100px 0px">Upload an image ►</button>
+							</form>
 						</ul>
 					</div>
 					
