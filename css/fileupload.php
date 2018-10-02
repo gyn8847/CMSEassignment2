01471ad64e1feb8094eaf320b8dddc83285c23f1
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-03 12:39:39 +1300

    Update fileupload.php

--- css/fileupload.php
+++ css/fileupload.php
@@ -5,7 +5,7 @@ $filepath = "images/" . $_FILES["file"]["name"];
  
 if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
 {
-echo "<img src=".$filepath." height=200 width=300 />";
+echo "<img src=".$filepath." min-height=200 min-width=300 />";
 } 
 else 
 {
