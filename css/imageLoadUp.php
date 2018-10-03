40fd1631748604850b94504cfc5c48427fb19b9f
gyn8847 <30676959+gyn8847@users.noreply.github.com>
2018-10-03 13:29:02 +1300

    Update imageLoadUp.php

--- css/imageLoadUp.php
+++ css/imageLoadUp.php
@@ -20,7 +20,7 @@ if(isset($_POST['submitImage']))
  
 	if(move_uploaded_file($_FILES["file"]["tmp_name"], $filepath)) 
 	{
-		echo "<img src=".$filepath." min-height=200 min-width=300 />";
+		echo "<img src=".$filepath." min-height=400 min-width=600 />";
 	} 
 	else 
 	{
